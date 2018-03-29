0bccc819fdd154b6f854b440c9df6aaf4c4bbc57
ajay <ajay.singh@tothenew.com>
2018-03-29 17:31:04 +0530

    Q1&Q2 for gorm

--- grails-app/init/linksharingajay/BootStrap.groovy
+++ grails-app/init/linksharingajay/BootStrap.groovy
@@ -7,6 +7,22 @@ class BootStrap {
     }
 
 
+    void createUsers(){
+        User user=new User()
+        user.email="asjodha@gamil.com"
+        user.userName="asj"
+        user.password="harry192"
+        user.firstName="ajay"
+        user.admin=true
+        user.save flush:true,failOnError:true
+        User user2=new User()
+        user2.email="asjodha22@gamil.com"
+        user2.userName="asj1928"
+        user2.password="harry1928"
+        user2.firstName="ajaySingh"
+        user2.admin=false
+        user2.save(flush:true,failOnError:true)
+    }
     def destroy = {
     }
 }
