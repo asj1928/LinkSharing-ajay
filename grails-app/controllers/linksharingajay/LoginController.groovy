f2ab4a127c398059a34eab722139a19bf838c78f
ajay <ajay.singh@tothenew.com>
2018-04-01 01:26:59 +0530

    till Q17 except[Q10,Q9,Q15]

--- grails-app/controllers/linksharingajay/LoginController.groovy
+++ grails-app/controllers/linksharingajay/LoginController.groovy
@@ -10,7 +10,22 @@ class LoginController {
         }
     }
 
-    def loginHandler(){}
+    def loginHandler(String userName,String password){
+        User user=User.findByUserNameAndPassword(userName,password)
+        if(user!=null){
+            if(user.active){
+                session.user=user
+                redirect action: 'index'
+
+            }else {
+                flash.error='Your accoutn is not active'
+            }
+        }
+        else{
+            flash.error="User not found"
+        }
+
+    }
 
     def logout(){
         session.invalidate()
