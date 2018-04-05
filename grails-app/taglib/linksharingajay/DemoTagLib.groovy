e915b960353b4562ffe03ab4bafa69dc0cc86c76
ajay <ajay.singh@tothenew.com>
2018-04-05 09:12:30 +0530

    taglib

---
+++ grails-app/taglib/linksharingajay/DemoTagLib.groovy
@@ -0,0 +1,6 @@
+package linksharingajay
+
+class DemoTagLib {
+    static defaultEncodeAs = [taglib:'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+}
---
+++ src/test/groovy/linksharingajay/DemoTagLibSpec.groovy
@@ -0,0 +1,18 @@
+package linksharingajay
+
+import grails.testing.web.taglib.TagLibUnitTest
+import spock.lang.Specification
+
+class DemoTagLibSpec extends Specification implements TagLibUnitTest<DemoTagLib> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
