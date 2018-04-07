8635e15921806b08245b97c8105bef4a4184a732
ajay <ajay.singh@tothenew.com>
2018-04-07 10:02:04 +0530

    testng

---
+++ src/test/groovy/linksharingajay/LoginCheckInterceptorSpec.groovy
@@ -0,0 +1,21 @@
+package linksharingajay
+
+import grails.testing.web.interceptor.InterceptorUnitTest
+import spock.lang.Specification
+
+class LoginCheckInterceptorSpec extends Specification implements InterceptorUnitTest<LoginCheckInterceptor> {
+    def setup() {
+    }
+
+    def cleanup() {
+
+    }
+
+    void "Test loginCheck interceptor matching"() {
+        when:"A request matches the interceptor"
+        withRequest(controller:"loginCheck")
+
+        then:"The interceptor does match"
+        interceptor.doesMatch()
+    }
+}
