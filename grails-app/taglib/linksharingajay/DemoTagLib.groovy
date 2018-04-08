ba895218e83792575ddf67b86f9394fa43dc3c48
ajay <ajay.singh@tothenew.com>
2018-04-08 22:08:35 +0530

    taglib Q2

--- grails-app/taglib/linksharingajay/DemoTagLib.groovy
+++ grails-app/taglib/linksharingajay/DemoTagLib.groovy
@@ -1,9 +1,9 @@
 package linksharingajay
 
 class DemoTagLib {
-    static defaultEncodeAs = [taglib:'html']
-    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
-    static namespace = "ls"
+        static defaultEncodeAs = [taglib:'html']
+        //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+        static namespace = "ls"
 
 
     def readLink = { attr, body ->
